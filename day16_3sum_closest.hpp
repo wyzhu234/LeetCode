920a434708b0ddba7ccc8de3998dfcded9fd85a1
wyzhu234 <18716423604@163.com>
2019-10-29 15:40:41 +0800

    day16，离目标最近的三数之和

---
+++ day16_3sum_closest.hpp
@@ -0,0 +1,50 @@
+#include "common.h"
+int threeSumClosest(vector<int>& nums, int target)
+{
+    if (nums.empty() || nums.size() < 3)
+    {
+        return INT_MAX;
+    }
+
+    int min_diff = INT_MAX;
+    int new_diff = 0;
+    int temp = 0;
+    int closest = 0;
+    sort(nums.begin(), nums.end());
+    for (size_t i = 0; i < nums.size() - 3; i++)
+    {
+        const int &fix = nums[i];
+        if (i > 0 && fix == nums[i - 1])
+        {
+            continue;
+        }
+
+        int l = i + 1;
+        int r = nums.size() - 1;
+        while (l < r)
+        {
+            temp = nums[i] + nums[l] + nums[r];
+            if (temp == target)
+            {
+                return temp;
+            }
+            else if (temp > target)
+            {
+                r--;
+            }
+            else
+            {
+                l++;
+            }
+
+            new_diff = abs(target - temp);
+            if (min_diff > new_diff)
+            {
+                min_diff = new_diff;
+                closest = temp;
+            }
+        }
+    }
+
+    return closest;
+}
