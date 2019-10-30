7ea6ce5c127c9759450251f0081802d815d29776
wyzhu234 <18716423604@163.com>
2019-10-30 16:45:31 +0800

    增加递归实现方式

--- day21_merge_two_sorted_lists.hpp
+++ day21_merge_two_sorted_lists.hpp
@@ -61,6 +61,21 @@ ListNode* mergeTwoLists(ListNode* l1, ListNode* l2)
     }
 }
 
+ListNode* mergeTwoLists_reverse(ListNode* l1, ListNode* l2)
+{
+    if (!l1) return l2;
+    if (!l2) return l1;
+    if (l1->val < l2->val)
+    {
+        l1->next = mergeTwoLists_reverse(l1->next, l2);
+        return l1;
+    }
+    else
+    {
+        l2->next = mergeTwoLists_reverse(l1, l2->next);
+        return l2;
+    }
+}
 
 void test_day21()
 {
@@ -75,5 +90,5 @@ void test_day21()
     l2 = l2->next;
     l2->next = new ListNode(4);
     //mergeTwoLists(head1, head2);
-    //ListNode * merged_node = mergeTwoLists_reverse(head1, head2);
+    ListNode * merged_node = mergeTwoLists_reverse(head1, head2);
 }
