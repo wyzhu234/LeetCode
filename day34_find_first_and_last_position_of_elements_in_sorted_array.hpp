84ce514540e95351f9c457a6d9a34d6daed1f8f4
wyzhu234 <18716423604@163.com>
2019-11-03 16:53:14 +0800

    添加两次调用二分查找法，寻找左右边界，注意实现细节。

--- day34_find_first_and_last_position_of_elements_in_sorted_array.hpp
+++ day34_find_first_and_last_position_of_elements_in_sorted_array.hpp
@@ -37,3 +37,50 @@ vector<int> searchRange(vector<int>& nums, int target)
     }
     return{ -1,-1 };
 }
+
+vector<int> searchRange_1(vector<int>& nums, int target)
+{
+    int left = 0;
+    //注意这里没有减1，下面循环的时候也没有了等号。
+    //有指针赋值时也没有减1了。
+    int right = nums.size();
+    vector<int> res(2,-1);
+    //寻找左侧的值
+    while (left < right)
+    {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] < target)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid;
+        }
+    }
+
+    if (right == nums.size() || nums[right] != target)
+    {
+        return res;
+    }
+
+    res[0] = right;
+    right = nums.size();
+    //寻找右侧
+    while (left < right)
+    {
+        int mid = left + (right - left) / 2;
+        //注意这里变成了等号
+        if (nums[mid] <= target)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid;
+        }
+    }
+
+    res[1] = right - 1;
+    return res;
+}
