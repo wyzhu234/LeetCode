407fa0df17aa632a47f556c8ff38ddc69b5bb007
wyzhu234 <18716423604@163.com>
2019-12-11 22:01:12 +0800

    添加未优化空间的代码

--- day62_unique_paths.hpp
+++ day62_unique_paths.hpp
@@ -13,3 +13,18 @@ int uniquePaths(int m, int n)
 
     return dp[n - 1];
 }
+
+int uniquePaths2(int m, int n)
+{
+    vector<vector<int>> dp(m, vector<int>(n,1));
+    dp[0][0] = 0;
+    for (int i = 1; i < m; i++)
+    {
+        for (int j = 1; j < n; j++)
+        {
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        }
+    }
+
+    return dp.back().back();
+}
