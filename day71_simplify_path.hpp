fb32618b7445c3e05c290e38633a7cb770b74117
wyzhu234 <18716423604@163.com>
2020-10-06 16:52:44 +0800

    简化路径： 1.字符串处理，stringstream 2.分段处理，遇到”.“则跳过，遇到“..”则删除前面字符

---
+++ day71_simplify_path.hpp
@@ -0,0 +1,33 @@
+#include "common.h"
+#include <sstream>
+
+string simplify_path(string path) {
+    vector<string> vec;
+    string res, temp;
+    istringstream iss(path);
+    while (getline(iss, temp, '/'))
+    {
+        if (temp.empty() || temp == ".") continue;
+        if (temp == ".." && !vec.empty())
+        {
+            vec.pop_back();
+        }
+        else
+        {
+            vec.push_back(temp);
+        }
+    }
+
+    for (string s : vec)
+    {
+        res += "/" + s;
+    }
+
+    return res.empty() ? "/" : res;
+}
+
+void test_day71()
+{
+    string path = "/a/b/../";
+    cout << simplify_path(path) << endl;
+}
